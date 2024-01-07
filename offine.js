1f2009eb3787052783fd1ebb84240be75cb9c499
lesysang1322002 <128148253+lesysang1322002@users.noreply.github.com>
2024-01-07 21:06:50 +0700

    update

--- offine.js
+++ offine.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["JDY33.html","md5sum_generated_hash"],["ble.png","md5sum_generated_hash"]];
+var precacheConfig = [["JDY33.html","md5sum_generated_hash"],["ble.png","md5sum_generated_hash"],["leanbot.js","md5sum_generated_hash"]];
 var cacheName = 'sw-precache-v2-web-lightbulb-' + (self.registration ? self.registration.scope : '');
 
 
